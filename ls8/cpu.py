a1803f57e7d4d2a605e3d0186f8bda4d1a18b51e
fadygouda <fadygouda123@gmail.com>
2020-12-08 20:27:22 -0800

    day3 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,47 +1,58 @@
 """CPU functionality."""
+
 import sys
-import os.path
+
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
 MUL = 0b10100010
+PUSH = 0b01000101
+
+POP = 0b01000110
 
 class CPU:
     """Main CPU class."""
+
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256
-        self.reg = [0] * 8
-        self.reg[7] = 0xF4
+
+        self.registers = [0] * 8
+        self.registers[7] = 0xF4
         self.pc = 0
-        self.ir = 0
-        self.mar = 0
-        self.mdr = 0
-        self.fl = 0
+        self.ram = [0] * 256
         self.halted = False
-    def load(self):
+        self.sp = 7
+
+
+    def load(self, filename):
         """Load a program into memory."""
-        if len(sys.argv) < 2:
-            print("usage: comp.py filename")
-            sys.exit(1)
+
         address = 0
-        
-        with open(sys.argv[1]) as f:
-            for line in f:
-                string_val = line.split("#")[0].strip()
-                if string_val == "":
+        # open the file
+        with open(filename) as my_file:
+            # go through each line to parse and get
+            # the instruction
+            for line in my_file:
+                # try and get the instruction/operand in the line
+                comment_split = line.split("#")
+                maybe_binary_number = comment_split[0]
+                try:
+                    x = int(maybe_binary_number, 2)
+                    self.ram_write(x, address)
+                    address += 1
+                except:
                     continue
-                v = int(string_val, 2)
-                self.ram[address] = v
-                address += 1
+            space_for_stack = 256 - len(filename)
+            memory = filename + [0] * space_for_stack
+            return memory
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.registers[reg_a] += self.registers[reg_b]
         elif op == MUL:
-            self.reg[reg_a] *= self.reg[reg_b]
+            self.registers[reg_a] *= self.registers[reg_b]
+            self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -50,6 +61,7 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
+
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
@@ -58,40 +70,45 @@ class CPU:
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
+
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.registers[i], end='')
+
         print()
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
     def run(self):
         """Run the CPU."""
-        while not self.halted:
-            # IR (Instruction Register) = value at memory address in PC (Program Counter)
-            ir = self.ram_read(self.pc)
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-            self.execute_instruction(ir, operand_a, operand_b)
-    def execute_instruction(self, instruction, operand_a, operand_b):
-        if instruction == HLT:
-            self.halted = True
-        elif instruction == PRN:
-            print(self.reg[operand_a])
-            self.pc += 2
-        elif instruction == LDI:
-            self.reg[operand_a] = operand_b
-            self.pc += 3
-        elif instruction == MUL:
-            self.alu(instruction, operand_a, operand_b)
-            self.pc += 3
-        else:
-            print("INVALID INSTRUCTION.")
-
-    def number_of_operands(self, instruction):
-        # Instruction layout: 'AABCDDDD'
-        # * AA = Number of operands for this opcode, 0-2
-        # INSTRUCTION = 0b10000010 # >> 6 --> 0b10 & 0b11 --> 0b10
-        return ((instruction >> 6) & 0b11) + 1
-
-    def ram_read(self, mar): # Memory Address Register
-        return self.ram[mar]
-    
-    def ram_write(self, mdr, mar): # Memory Data Register, Memory Address Register
-        self.ram[mar] = mdr
+        running = True
+        while running:
+            
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc+1)
+            operand_b = self.ram_read(self.pc+2)
+            if IR == HLT:
+                self.halted = True
+                self.pc += 1
+            elif IR == PRN:
+                print(self.registers[operand_a])
+                self.pc += 2
+            elif IR == LDI:
+                self.registers[operand_a] = operand_b
+                self.pc += 3
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+            elif IR == PUSH:
+                self.registers[self.sp] -= 1
+                reg_address = self.ram[self.pc+1]
+                self.ram[self.registers[self.sp]] = self.registers[reg_address]
+                self.pc += 2
+            elif IR == POP:
+                reg_address = self.ram[self.pc+1]
+                self.registers[reg_address] = self.ram[self.registers[self.sp]]
+                self.registers[self.sp] += 1
+                self.pc += 2
